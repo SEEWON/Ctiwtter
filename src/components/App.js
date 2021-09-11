b294c7ec67be15866344afe1ef00f6eb6654b631
SEEWON <andyaaa78@gmail.com>
2021-09-12 00:30:12 +0900

    사진 업로드 미리보기 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,25 @@
-import React, { useEffect, useState } from "react"
-import AppRouter from "components/Router"
-import { authService } from "fbase"
-import { onAuthStateChanged } from "firebase/auth"
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [init, setInit] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [userObj, setUserObj] = useState(null)
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     onAuthStateChanged(authService, (user) => {
       if (user) {
-        setIsLoggedIn(true)
-        setUserObj(user)
+        setIsLoggedIn(true);
+        setUserObj(user);
       } else {
-        setIsLoggedIn(false)
+        setIsLoggedIn(false);
       }
-      setInit(true)
-    })
-  }, [])
+      setInit(true);
+    });
+  }, []);
 
-  return (
-    <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
-      <footer>&copy; Ctwitter {new Date().getFullYear()}</footer>
-    </>
-  )
+  return <>{init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}</>;
 }
 
-export default App
+export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 const Home = ({ userObj }) => {
   const [ctweet, setCtweet] = useState("");
   const [ctweets, setCtweets] = useState([]);
+  const [attachment, setAttachment] = useState();
   useEffect(() => {
     onSnapshot(collection(dbService, "ctweets"), (snapshot) => {
       const ctweetArray = snapshot.docs.map((doc) => ({
@@ -30,11 +31,33 @@ const Home = ({ userObj }) => {
     } = event;
     setCtweet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setAttachment(null);
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={ctweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Ctweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} alt="" width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {ctweets.map((ctweet) => (
